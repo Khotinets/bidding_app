6f7ae1225a693859a9251e16aad13b77a0e0c5ba
Liubomyr Khotinets <l.khotinets@gmail.com>
2018-01-12 15:34:30 +0000

    add categories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110184914) do
+ActiveRecord::Schema.define(version: 20180111220206) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "name"
