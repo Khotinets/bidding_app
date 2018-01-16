df6f1a82e303d1cf1bfef8b221a0634f37303936
Liubomyr Khotinets <l.khotinets@gmail.com>
2018-01-16 09:06:56 +0000

    update categories function, forms stylization

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,23 +9,38 @@ class CategoriesController < ApplicationController
     end
     
     def create
-        @category = Category.new(category_params)
-        if @category.save
-            redirect_to @category, notice: 'Category was successfully created.'
-        else
-            render 'new'
+    @category = Category.new(category_params)
+        respond_to do |format|
+            if @category.save
+                format.html { redirect_to @category, notice: 'Category was successfully created.' }
+                format.json { render :show, status: :created, location: @category }
+            else
+                format.html { render :new }
+                format.json { render json: @category.errors, status: :unprocessable_entity }
+            end
         end
     end
     
     def edit
-        
+        @category = Category.find(params[:id])
+    end
+    
+    def update
+        @category = Category.find(params[:id])
+        if @category.update_attributes(category_params)
+            redirect_to @category
+        else
+            render 'edit'
+        end
     end
     
     def show
         @category = Category.find(params[:id])
     end
     private
-    
+        def set_category
+            @category = Category.find(params[:id])
+        end
         def category_params
           params.require(:category).permit(:name, :description)
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   
-  resources :auctions
-  resources :products
+ resources :auctions
+ resources :products
  devise_for :users, controllers: { registrations: "registrations" }
  devise_scope :user do  
-  get '/users/sign_out' => 'devise/sessions#destroy'     
+ get '/users/sign_out' => 'devise/sessions#destroy'     
  end
 
  resources  :profiles
  resources  :categories
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
  root 'static_pages#index'
  
 end
