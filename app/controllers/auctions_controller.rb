6e94ed56ade72260c2878c0c33fa3b1e1fd71295
Liubomyr Khotinets <l.khotinets@gmail.com>
2018-02-16 19:52:37 +0000

    Only owner can add product to auction, some refactor

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,5 +1,6 @@
 class AuctionsController < ApplicationController
-  before_action :set_auction, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_auction, only: [:show, :destroy]
 
   # GET /auctions
   # GET /auctions.json
@@ -16,38 +17,23 @@ class AuctionsController < ApplicationController
   def new
     @auction = Auction.new
   end
-
-  # GET /auctions/1/edit
-  def edit
-  end
-
+  
   # POST /auctions
   # POST /auctions.json
   def create
     @auction = Auction.new(auction_params)
-
-    respond_to do |format|
-      if @auction.save
-        format.html { redirect_to @auction, notice: 'Auction was successfully created.' }
-        format.json { render :show, status: :created, location: @auction }
-      else
-        format.html { render :new }
-        format.json { render json: @auction.errors, status: :unprocessable_entity }
+    if current_user.id == @auction.user_id
+      respond_to do |format|
+        if @auction.save
+          format.html { redirect_to @auction, notice: 'Auction was successfully created.' }
+          format.json { render :show, status: :created, location: @auction }
+        else
+          format.html { render :new }
+          format.json { render json: @auction.errors, status: :unprocessable_entity }
+        end
       end
-    end
-  end
-
-  # PATCH/PUT /auctions/1
-  # PATCH/PUT /auctions/1.json
-  def update
-    respond_to do |format|
-      if @auction.update(auction_params)
-        format.html { redirect_to @auction, notice: 'Auction was successfully updated.' }
-        format.json { render :show, status: :ok, location: @auction }
-      else
-        format.html { render :edit }
-        format.json { render json: @auction.errors, status: :unprocessable_entity }
-      end
+    else
+      redirect_to products_path, notice: 'You are not the owner of the product'
     end
   end
 
@@ -64,7 +50,7 @@ class AuctionsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_auction
-      @auction = Auction.find(params[:id])
+      @auction = current_user.auctions.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,10 @@
 module ProductsHelper
     
+    #Check if user is product owner
+    def product_owner?
+       @product.user_id == current_user.id
+    end
+    
     #Rendering like or unlike button
     def choose_fav_button(product)
         if user_signed_in?
